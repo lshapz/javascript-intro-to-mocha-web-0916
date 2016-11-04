4e8a8e670c0eb629c7371d3cbb0f21a969765ea6
lshapz <laurashapiro01@gmail.com>
2016-11-04 17:01:48 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+function favoriteIceCream(flavor){
+
+  return `I love ${flavor}`
+}
+
+function sayHeyFriend(name){
+
+  return `Hey, ${name}!`
+}
+
+function shouting(string){
+
+  return string.toUpperCase()
+}
+
+function roundDown(number){
+
+  return Math.floor(number)
+}
